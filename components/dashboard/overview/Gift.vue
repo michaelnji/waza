7dca26f634b66e1c36175a7f70a81b5d325bb460
COD3VIL <codebase65@outlook.com>
2024-07-11 22:07:56 +0100

    feat: 💄 building ui and making improvements

--- components/dashboard/overview/Gift.vue
+++ components/dashboard/overview/Gift.vue
@@ -7,7 +7,7 @@
         <h2 class="font-medium text-3xl md:text-4xl font-display text-stone-50">Send airtime to a friend today!
         </h2>
         <Button size="lg"
-            class="px6 py2 mt-4 shadow-none rounded-full text-stone-50 font-400 text-xl  bg-stone-900 hover:bg-stone-800 active:scale-95 transition ease-in">
+            class="px6 py2 mt-4 shadow-none rounded-full text-stone-50 font-400 text-xl  bg-stone-900 hover:bg-stone-800 active:scale-95 transition ease-in font-display">
 
             Gift Airtime
         </Button>
